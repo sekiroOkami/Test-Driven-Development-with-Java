62ad320618f1877713bea2fca73ff2b56c6a3b8e
global <global@mail.com>
2024-03-11 16:08:11 +0700

    start ch9

--- DIY/chapter5/src/test/java/com/example/WordSelectionTest.java
+++ DIY/chapter5/src/test/java/com/example/WordSelectionTest.java
@@ -1,2 +1,9 @@
-package com.example;public class WordSelectionTest {
+package com.example;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class WordSelectionTest {
 }
